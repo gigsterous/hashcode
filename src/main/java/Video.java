3d9e8c3c44ba89221158633fca5827c9ffa40097
Michal Švácha <michal.svacha@gmail.com>
2017-02-23 19:10:35 +0100

    Video object

---
+++ src/main/java/Video.java
@@ -0,0 +1,21 @@
+/**
+ * Created by michalsvacha on 23.02.17.
+ */
+
+public class Video {
+    private int id;
+    private int size;
+
+    public Video(int id, int size) {
+        this.id = id;
+        this.size = size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
