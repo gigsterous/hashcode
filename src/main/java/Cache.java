38b1fd6fb4c88cccde88af81fc69ab214ac8ad13
Martin Myslik <m.myslik@gmail.com>
2017-02-23 20:01:00 +0100

    Video helper methods

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by smajl on 23.2.17.
@@ -10,24 +8,30 @@ public class Cache {
 
     private int id;
     private int capacity;
-    private List<Video> videos;
-    private Set<Integer> videoIds;
+    private Map<Integer, Video> videos;
 
     public Cache(int id, int capacity) {
         this.id = id;
         this.capacity = capacity;
 
-        videos = new ArrayList<Video>();
-        videoIds = new HashSet<Integer>();
+        videos = new HashMap<Integer, Video>();
     }
 
     public boolean containsVideo(int videoId) {
-        return videoIds.contains(videoId);
+        return videos.containsKey(videoId);
+    }
+
+    public void addVideo(Video video) {
+        videos.put(video.getId(), video);
+    }
+
+    public void removeVideo(Video video) {
+        videos.remove(video.getId());
     }
 
     public int remainingCapacity() {
         int usage = 0;
-        for (Video v: videos) {
+        for (Video v: videos.values()) {
             usage += v.getSize();
         }
         return capacity - usage;
