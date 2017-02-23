19012b41a705aa90c0e269b146078524f9207e37
Michal Švácha <michal.svacha@gmail.com>
2017-02-23 20:37:56 +0100

    Deleted comparator

--- src/main/java/Video.java
+++ src/main/java/Video.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
  * Created by michalsvacha on 23.02.17.
  */
 
-public class Video implements Comparator<Video> {
+public class Video {
 
     private int id;
     private int size;
@@ -13,8 +13,6 @@ public class Video implements Comparator<Video> {
     public Video(int id, int size) {
         this.id = id;
         this.size = size;
-
-        attractivity = 0;
     }
 
     public int getId() {
@@ -25,23 +23,11 @@ public class Video implements Comparator<Video> {
         return size;
     }
 
-    public int getAttractivity() {
-        return attractivity;
-    }
-
-    public void addAttractivity(int attractivity) {
-        this.attractivity += attractivity;
-    }
-
     @Override
     public String toString() {
         return "Video {" +
                 "id = " + id +
                 ", size = " + size +
                 '}';
-    }
-
-    public int compare(Video o1, Video o2) {
-        return o2.getAttractivity() - o1.getAttractivity();
     }
 }
